d2dde867a4734043bbe5658027868829ae918279
NickAckerman16 <sanchez.chipana.nicole@itdonbosco.org>
2025-04-30 13:25:54 -0400

    Actualizacon de Clase

--- app.py
+++ app.py
@@ -19,5 +19,9 @@ def list_tasks():
 def login():
     return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5001)
